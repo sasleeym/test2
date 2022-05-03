252f6cc0dbe68dbf659ffeb46b4600b0488c7a2f
sasleeym <104290503+sasleeym@users.noreply.github.com>
2022-05-03 10:44:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+})
 basic.forever(function () {
 	
 })
